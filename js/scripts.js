6320c09515caa95f2293f6ce5fb0df3dc10ac0af
Alex <kirov_as@i.ua>
2015-09-20 18:54:35 +0300

    commit 1

---
+++ js/scripts.js
@@ -0,0 +1,240 @@
+$(document).ready(function() {
+
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true
+	});
+	// animation
+	$('.section_header').waypoint(function(down) {
+		$(this).addClass('animation');
+		$(this).addClass('fadeInUp');
+	}, { offset: '100%' });
+	
+	$('.feature, .plan').waypoint(function(down) {
+		$(this).addClass('animation');
+		$(this).addClass('flipInY');
+	}, { offset: '100%' });
+	
+	$('.purchase_i, .purchase_title, .p-btn').waypoint(function(down) {
+		$(this).addClass('animation');
+		$(this).addClass('fadeInLeft');
+	}, { offset: '100%' });
+	
+	$('.team_item').waypoint(function(down) {
+		$(this).addClass('animation');
+		$(this).addClass('flipInX');
+	}, { offset: '100%' });
+	$('.animation1').waypoint(function(down) {
+		$(this).addClass('animation');
+		$(this).addClass('fadeInUp');
+	}, { offset: '100%' });
+	
+
+	function heightDetect() {
+		$(".top").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a.link").attr("href", "#work_" + i);
+		$(this).find(".podrt_descr").attr("id", "work_" + i);
+	});
+
+	$("input, select, textarea").jqBootstrapValidation();
+
+	$(".top_mnu ul a").mPageScroll2id();
+
+	
+});
+// video background
+$(document).ready(function(){
+			$('.video').YTPlayer({
+  videoId: 'jktC0pZf7RA',
+  callback: function() {
+    console.log("playerFinshed");
+  }
+});
+		});
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	$(".top_text p").animated("fadeInUp", "fadeOutDown");
+
+}); 
+
+jQuery(document).ready(function($) {
+            $('.counter_number').counterUp({
+                delay: 10,
+                time: 1000
+            });
+        });
+		
+function initialize() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+          center: new google.maps.LatLng(44.5403, -78.5463),
+          zoom: 8,
+		  scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+		map.set('styles', [
+  {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 65
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 51
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 30
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 40
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffff00"
+            },
+            {
+                "lightness": -25
+            },
+            {
+                "saturation": -97
+            }
+        ]
+    }
+]);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
